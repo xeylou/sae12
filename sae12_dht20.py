09e8e40fc2b56499c7c260a3792af16813308457
xeylou <71969414+xeylou@users.noreply.github.com>
2023-01-01 22:03:58 +0000

    sync w overleaf

--- sae12_dht20.py
+++ sae12_dht20.py
@@ -6,14 +6,16 @@ while True:
     try:
         temperature, humidity = dht_device.temperature, dht_device.humidity 
         # Recupere les donnees du capteur
+        
         print("Temperature: " + str(temperature) + " C, Humidite: " + str(humidity) + "%", end="\r") 
-        # Affiche ces donnees dans la console
-    except RuntimeError: 
-    # Il arrive que les donnees du capteur arrivent dans le Raspberry en etant corrompues, on ignore donc cette erreur
-        time.sleep(2.0) # Pause de 2 secondes
-        continue # On continue la boucle
-    except Exception as error: 
-    # Cette erreur est consideree comme grave, elle n'est pas censee arriver mais protection par precaution
-        dht_device.exit() # Ferme proprement la liaison capteur-programme si erreur
-        raise error # Laisse l'erreur interrompre le programme
-    time.sleep(2.0) # Raffraichissement toutes les 2 secondes 
+        # Affiche les donnees sur la console
+        
+    except RuntimeError:                          # Prevention contre donnees du capteur corrompues 
+        time.sleep(2.0)                             # Pause de 2 secondes
+        continue                                     # Reprise de la boucle
+        
+    except Exception as error:                  # Precaution contre erreur grave extremement rare
+        dht_device.exit()                          # Ferme proprement la liaison capteur-programme si erreur
+        raise error                                  # Laisse l'erreur interrompre le programme
+        
+    time.sleep(2.0)                                 # Raffraichissement toutes les 2 secondes 
